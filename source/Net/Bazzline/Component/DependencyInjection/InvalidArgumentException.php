3d08bbe267938d36d823b534597a179f1b9b5742
stevleibelt <stev@leibelt.de>
2013-07-29 23:04:23 +0200

    Added exception

---
+++ source/Net/Bazzline/Component/DependencyInjection/InvalidArgumentException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-29 
+ */
+
+namespace Net\Bazzline\Component\DependencyInjection;
+
+use InvalidArgumentException as ParentClass;
+
+/**
+ * Class InvalidArgumentException
+ *
+ * @package Net\Bazzline\Component\DependencyInjection
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-29
+ */
+class InvalidArgumentException extends ParentClass {}
