181f4cb5014891fc0666ccfa4658af8255f4b5b2
stevleibelt <stev@leibelt.de>
2013-07-29 23:18:07 +0200

    Added method call

---
+++ source/Net/Bazzline/Component/DependencyInjection/MethodCall.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-29 
+ */
+
+namespace Net\Bazzline\Component\DependencyInjection;
+
+/**
+ * Class MethodCall
+ *
+ * @package Net\Bazzline\Component\DependencyInjection
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-07-29
+ */
+interface MethodCall
+{
+    /**
+     * @param mixed $value
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-29
+     */
+    public function addParameter($value);
+
+    /**
+     * @return string
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-29
+     */
+    public function getMethodName();
+
+    /**
+     * @return array
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-29
+     */
+    public function getParameters();
+
+    /**
+     * @param string $name
+     * @return $this
+     * @throws InvalidArgumentException - empty string provided
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-29
+     */
+    public function setMethodName($name);
+
+    /**
+     * @param array $values
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-07-29
+     */
+    public function setParameters(array $values);
+}
