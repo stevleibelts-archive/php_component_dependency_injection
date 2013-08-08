ac628c7a681ad5eb2958ef72ad3844061da37a5d
stevleibelt <stev@leibelt.de>
2013-08-09 00:04:11 +0200

    Created environment for examples

---
+++ example/Example/UseContainerAsFactory/Example.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-09 
+ */
+
+namespace Example\UseContainerAsFactory;
+
+/**
+ * Class Example
+ *
+ * @package Example\UseContainerAsFactory
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-09
+ */
+class Example
+{
+
+}
