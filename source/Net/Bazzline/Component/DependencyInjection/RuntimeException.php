70d86b4cf54d5177b789b2a8b61aad363f63b631
stevleibelt <stev@leibelt.de>
2013-08-10 23:49:30 +0200

    Created runtime exception

---
+++ source/Net/Bazzline/Component/DependencyInjection/RuntimeException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-10 
+ */
+
+namespace Net\Bazzline\Component\DependencyInjection;
+
+use RuntimeException as ParentClass;
+
+/**
+ * Class RuntimeException
+ *
+ * @package Net\Bazzline\Component\DependencyInjection
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-10
+ */
+class RuntimeException extends ParentClass {}
