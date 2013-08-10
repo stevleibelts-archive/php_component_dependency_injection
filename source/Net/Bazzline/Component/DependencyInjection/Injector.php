77c76730e75e208df2509b3ad23bd43ca5e6c2d4
stevleibelt <stev@leibelt.de>
2013-08-10 23:43:49 +0200

    Updates injector interface

--- source/Net/Bazzline/Component/DependencyInjection/Injector.php
+++ source/Net/Bazzline/Component/DependencyInjection/Injector.php
@@ -15,8 +15,44 @@ namespace Net\Bazzline\Component\DependencyInjection;
  */
 class Injector implements InjectorInterface
 {
+    /**
+     * Adds a class to the injector. If you do not provide a declaration, the
+     *  injector simple creates a new instance of that class by each call.
+     *
+     * @param string $className
+     * @param DeclarationInterface $definition
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-10
+     */
     public function addConsumer($className, DeclarationInterface $definition = null)
     {
+        // TODO: Implement addConsumer() method.
+    }
 
+    /**
+     * Validates if given class name where add as consumer to the injector.
+     *
+     * @param string $className
+     * @return boolean
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-09-10
+     */
+    public function hasConsumer($className)
+    {
+        // TODO: Implement hasConsumer() method.
+    }
+
+    /**
+     * Returns a new or shared instance of a given class name.
+     *
+     * @param string $className
+     * @return null|mixed|object
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-10
+     */
+    public function getConsumer($className)
+    {
+        // TODO: Implement getConsumer() method.
     }
 }
--- source/Net/Bazzline/Component/DependencyInjection/InjectorInterface.php
+++ source/Net/Bazzline/Component/DependencyInjection/InjectorInterface.php
@@ -26,4 +26,24 @@ interface InjectorInterface
      * @since 2013-08-10
      */
     public function addConsumer($className, DeclarationInterface $definition = null);
+
+    /**
+     * Validates if given class name where add as consumer to the injector.
+     *
+     * @param string $className
+     * @return boolean
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-09-10
+     */
+    public function hasConsumer($className);
+
+    /**
+     * Returns a new or shared instance of a given class name.
+     *
+     * @param string $className
+     * @return null|mixed|object
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-10
+     */
+    public function getConsumer($className);
 }
