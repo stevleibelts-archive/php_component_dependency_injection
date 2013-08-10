521ac7d01fb339ed40fe893d010645ac188f468d
stevleibelt <stev@leibelt.de>
2013-08-10 23:33:30 +0200

    Started injector

---
+++ source/Net/Bazzline/Component/DependencyInjection/Injector.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-10 
+ */
+
+namespace Net\Bazzline\Component\DependencyInjection;
+
+class Injector implements InjectorInterface
+{
+    public function addClass($className, DefinitionInterface $definition = null)
+    {
+
+    }
+}
---
+++ source/Net/Bazzline/Component/DependencyInjection/InjectorInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-10 
+ */
+
+namespace Net\Bazzline\Component\DependencyInjection;
+
+/**
+ * Class InjectorInterface
+ *
+ * @package Net\Bazzline\Component\DependencyInjection
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-10
+ */
+interface InjectorInterface
+{
+    public function addClass($className, DefinitionInterface $definition = null);
+}
