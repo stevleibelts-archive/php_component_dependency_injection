98bded87b285b7593888443b9f58e0780ec0dc35
stevleibelt <stev@leibelt.de>
2013-08-10 23:39:42 +0200

    Added phpdoc

--- source/Net/Bazzline/Component/DependencyInjection/Injector.php
+++ source/Net/Bazzline/Component/DependencyInjection/Injector.php
@@ -6,9 +6,16 @@
 
 namespace Net\Bazzline\Component\DependencyInjection;
 
+/**
+ * Class Injector
+ *
+ * @package Net\Bazzline\Component\DependencyInjection
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-10
+ */
 class Injector implements InjectorInterface
 {
-    public function addClass($className, DeclarationInterface $definition = null)
+    public function addConsumer($className, DeclarationInterface $definition = null)
     {
 
     }
--- source/Net/Bazzline/Component/DependencyInjection/InjectorInterface.php
+++ source/Net/Bazzline/Component/DependencyInjection/InjectorInterface.php
@@ -15,5 +15,15 @@ namespace Net\Bazzline\Component\DependencyInjection;
  */
 interface InjectorInterface
 {
-    public function addClass($className, DeclarationInterface $definition = null);
+    /**
+     * Adds a class to the injector. If you do not provide a declaration, the
+     *  injector simple creates a new instance of that class by each call.
+     *
+     * @param string $className
+     * @param DeclarationInterface $definition
+     * @return $this
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-10
+     */
+    public function addConsumer($className, DeclarationInterface $definition = null);
 }
