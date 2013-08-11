c886d2c41e45f4a4b452d7f27c27e1ea498c06d0
stevleibelt <stev@leibelt.de>
2013-08-11 20:56:31 +0200

    Created testcase

---
+++ test/Test/Net/Bazzline/Component/DependencyInjection/TestCase.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-11 
+ */
+
+namespace Net\Bazzline\Component\DependencyInjection;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Class TestCase
+ *
+ * @package Net\Bazzline\Component\DependencyInjection
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-11
+ */
+class TestCase extends PHPUnit_Framework_TestCase
+{
+
+}
