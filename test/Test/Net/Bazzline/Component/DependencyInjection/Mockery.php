52f0b81d2ea8962da3db0a58b5905a956384c429
stevleibelt <stev@leibelt.de>
2013-08-12 00:16:09 +0200

    added mockery extension

---
+++ test/Test/Net/Bazzline/Component/DependencyInjection/Mockery.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-12 
+ */
+
+namespace Test\Net\Bazzline\Component\DependencyInjection;
+
+use Mockery as ParentClass;
+
+/**
+ * Class Mockery
+ *
+ * @package Test\Net\Bazzline\Component\DependencyInjection
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-12
+ */
+class Mockery extends ParentClass {}
