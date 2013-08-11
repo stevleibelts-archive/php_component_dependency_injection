4ecb26c51bf0ec49868ea09206000398695b926f
stevleibelt <stev@leibelt.de>
2013-08-12 00:30:32 +0200

    Added IsNotSharedDefinition

---
+++ source/Net/Bazzline/Component/DependencyInjection/IsNotSharedDefinition.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-12 
+ */
+
+namespace Net\Bazzline\Component\DependencyInjection;
+
+/**
+ * Class IsNotSharedDefinition
+ *
+ * @package Net\Bazzline\Component\DependencyInjection
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-08-12
+ */
+class IsNotSharedDefinition extends Definition
+{
+    /**
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-08-12
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setShared(false);
+    }
+}
